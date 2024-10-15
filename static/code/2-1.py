3672a7538920b1179fe80b82d35faa63b74cbacc
haekyu.cho <haekyu.cho@nhn.com>
2024-10-15 20:32:30 +0900

    blog

--- static/code/2-1.py
+++ static/code/2-1.py
@@ -1,4 +1,6 @@
-# 예제 2.1 토큰화 코드
+import torch
+import torch.nn as nn
+
 # 띄어쓰기 단위로 분리
 input_text = "나는 최근 파리 여행을 다녀왔다"
 input_text_list = input_text.split()
@@ -13,3 +15,10 @@ print("idx2str: ", idx2str)
 # 토큰을 토큰 아이디로 변환
 input_ids = [str2idx[word] for word in input_text_list]
 print("input_ids: ", input_ids)
+
+embedding_dim = 16
+embed_layer = nn.Embedding(len(str2idx), embedding_dim)
+
+input_embeddings = embed_layer(torch.tensor(input_ids))  # (5, 16)
+input_embeddings = input_embeddings.unsqueeze(0)  # (1, 5, 16)
+input_embeddings.shape
