a42dce502d1e8178c9f2efb5825e1e998ab17711
haekyu.cho <haekyu.cho@nhn.com>
2025-06-23 22:02:17 +0900

    add docusaurus search plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="아윤이 아빠의 기술 블로그입니다.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
